b46cd9caf585f5f531d17a3d21d45211d7ce31b5
vlad vusaty <vladel9ts@gmail.com>
2024-06-24 15:20:23 +0300

    add first codegen

---
+++ main.py
@@ -0,0 +1,27 @@
+import re
+import time
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_placeholder("What needs to be done?").click()
+    page.get_by_placeholder("What needs to be done?").fill("Первый список")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+    page.get_by_placeholder("What needs to be done?").fill("Второй Список")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+    page.get_by_placeholder("What needs to be done?").fill("Третий список")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+    time.sleep(5)
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
