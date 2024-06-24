f6cc8e6e1bf1db034e12cb2fac5a13f20a7b3386
vlad9vlav9 <vlad9slav9@gmail.com>
2024-06-24 22:50:01 +0300

    add conftest and locators

--- test_todo.py
+++ test_todo.py
@@ -4,10 +4,7 @@ import time
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+def test_add_todo(page):
     page.goto("https://demo.playwright.dev/todomvc/#/")
     page.get_by_placeholder("What needs to be done?").click()
     page.get_by_placeholder("What needs to be done?").fill("Первый список")
@@ -16,12 +13,19 @@ def run(playwright: Playwright) -> None:
     page.get_by_placeholder("What needs to be done?").press("Enter")
     page.get_by_placeholder("What needs to be done?").fill("Третий список")
     page.get_by_placeholder("What needs to be done?").press("Enter")
-    time.sleep(5)
+    #time.sleep(3)
 
-    # ---------------------
-    context.close()
-    browser.close()
+def test_loc(page):
+    page.goto('https://zimaev.github.io/text_input/')
+    page.get_by_label("Email address").fill("qa@example.com")
+    page.get_by_title("username").fill("Anton")
+    page.get_by_placeholder('password').fill("secret")
+    page.get_by_role('checkbox').click()
+    #time.sleep(3)
 
-
-with sync_playwright() as playwright:
-    run(playwright)
+def test_checkbox_l(page):
+    page.goto('https://zimaev.github.io/checks-radios/')
+    checkboxes = page.locator("input")
+    for checkbox in checkboxes.all():
+        checkbox.check()
+    time.sleep(3)
