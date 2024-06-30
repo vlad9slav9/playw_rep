4ff37a522942d4c8eb4d97b0b419e2eba218f457
vlad9vlav9 <vlad9slav9@gmail.com>
2024-06-30 22:03:55 +0300

    add full test fill form

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import pytest
 @pytest.fixture(scope="session")
 def browser(playwright):
     # Укажите путь к исполняемому файлу Яндекс.Браузера
-    yandex_browser_path = "C:\\Users\\KSK-SHOP\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe"  # Замените на реальный путь
+    yandex_browser_path = "C:\\Users\\ASUS\\AppData\\Local\\Chromium\\Application\\chrome.exe"  # Замените на реальный путь
 
     browser = playwright.chromium.launch(executable_path=yandex_browser_path, headless=False)
 
--- test_fill_form.py
+++ test_fill_form.py
@@ -1,10 +1,24 @@
 import time
+from playwright.sync_api import expect
 
 
-def test_example(page):
+def test_todo(page):
     page.goto("https://demo.playwright.dev/todomvc/#/")
-    time.sleep(5)
+    expect(page).to_have_url("https://demo.playwright.dev/todomvc/#/")
 
+    input_field = page.get_by_placeholder('What needs to be done?')
+    expect(input_field).to_be_empty()
 
+    input_field.fill("Закончить курс по playwright")
+    input_field.press("Enter")
+
+    input_field.fill('Добавить в резюме, что умею программировать')
+    input_field.press('Enter')
+
+    todo_item = page.get_by_test_id('todo-item')
+    expect(todo_item).to_have_count(2)
+
+    todo_item.get_by_role('checkbox').nth(0).click()
+    expect(todo_item.nth(0)).to_have_class('completed')
 
 
